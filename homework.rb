2be7390e3213fe97cc9d705164e3703c073587b5
Stephan Constantial <Sconstantial@yahoo.com>
2018-05-17 08:56:54 +0100

    Homework commit

---
+++ homework.rb
@@ -0,0 +1,14 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket"]
+stops.push ("Edinburgh Waverley")
+p stops
+stops.unshift("Glasgow Queen st.")
+p stops
+stops.insert(4, "Polmont")
+p stops
+ p stops.index("Linlithgow")
+ stops.delete("Cumbernauld")
+ p stops
+ p stops.count
+ p stops[2]
+ p stops [-6]
+p stops.reverse
